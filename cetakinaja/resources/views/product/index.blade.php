b140eb8d9bba952b1e0067d57e5d27e3fa7ff657
unknown <you@example.comalbertfss14@gmail.com>
2021-06-28 12:50:31 +0700

    update

--- cetakinaja/resources/views/product/index.blade.php
+++ cetakinaja/resources/views/product/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row" style="margin-top: 5rem;">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Review</h2>
+                <h2>Product</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('product.create') }}"> Create New Product</a>
